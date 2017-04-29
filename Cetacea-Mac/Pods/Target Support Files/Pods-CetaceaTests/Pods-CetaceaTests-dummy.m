11027e4e89927bcf4b74cddb4e942b9a53f949c0
Justin Fincher <zhtsu47@me.com>
2017-04-30 03:13:43 +0800

    Fix Project Structure

--- Cetacea-Mac/Pods/Target Support Files/Pods-CetaceaTests/Pods-CetaceaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CetaceaTests : NSObject
-@end
-@implementation PodsDummy_Pods_CetaceaTests
-@end
--- Cetacea-Mac/Pods/Target Support Files/Pods-CetaceaUITests/Pods-CetaceaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CetaceaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_CetaceaUITests
-@end
