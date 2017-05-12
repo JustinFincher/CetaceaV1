5605824b6eb699159f8a9ed28ac6bed319fcb8d2
Justin Fincher <zhtsu47@me.com>
2017-05-12 23:06:06 +0800

    Commit before branch

---
+++ Cetacea/CetaceaSharedFramework/CSFEditorTextView.h
@@ -0,0 +1,21 @@
+//
+//  CSFEditorTextView.h
+//  CetaceaSharedFramework
+//
+//  Created by Justin Fincher on 2017/5/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#if TARGET_OS_IOS
+#import <UIKit/UIKit.h>
+#define EditorTextViewClass UITextView
+#elif TARGET_OS_OSX
+#import <AppKit/AppKit.h>
+#define EditorTextViewClass NSTextView
+#endif
+
+@interface CSFEditorTextView : EditorTextViewClass
+
+@end
---
+++ Cetacea/CetaceaSharedFramework/CSFEditorTextView.m
@@ -0,0 +1,21 @@
+//
+//  CSFEditorTextView.m
+//  CetaceaSharedFramework
+//
+//  Created by Justin Fincher on 2017/5/12.
+//
+//
+
+#import "CSFEditorTextView.h"
+
+@implementation CSFEditorTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
