f5f7eff431cb577b12a974c5d5472503bed2638c
Justin Fincher <zhtsu47@me.com>
2017-05-19 16:19:13 +0800

    Well then

---
+++ Cetacea/CetaceaSharedFramework/CSFTextAttributes.m
@@ -0,0 +1,13 @@
+//
+//  CSFTextAttributes.m
+//  CetaceaSharedFramework
+//
+//  Created by Justin Fincher on 2017/5/19.
+//
+//
+
+#import "CSFTextAttributes.h"
+
+@implementation CSFTextAttributes
+
+@end
--- Cetacea/Pods/CocoaMarkdown/CocoaMarkdown/CMDocument.m
+++ Cetacea/Pods/CocoaMarkdown/CocoaMarkdown/CMDocument.m
@@ -13,32 +13,51 @@
 
 - (instancetype)initWithData:(NSData *)data options:(CMDocumentOptions)options
 {
-    NSParameterAssert(data);
-    
-    if ((self = [super init])) {
-        cmark_node *node = cmark_parse_document((const char *)data.bytes, data.length, options);
-        if (node == NULL) return nil;
-        
-        _rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
-        _options = options;
-    }
-    return self;
+	NSParameterAssert(data);
+	
+	if ((self = [super init])) {
+		cmark_node *node = cmark_parse_document((const char *)data.bytes, data.length, options);
+		if (node == NULL) return nil;
+		
+		_rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
+		_options = options;
+	}
+	return self;
 }
-
+- (void)updateWithData:(NSData *)data options:(CMDocumentOptions)options
+{
+	NSParameterAssert(data);
+	cmark_node *node = cmark_parse_document((const char *)data.bytes, data.length, options);
+	if (node == NULL) return;
+	
+	_rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
+	_options = options;
+}
 - (instancetype)initWithContentsOfFile:(NSString *)path options:(CMDocumentOptions)options
 {
-    if ((self = [super init])) {
-        FILE *fp = fopen(path.UTF8String, "r");
-        if (fp == NULL) return nil;
-        
-        cmark_node *node = cmark_parse_file(fp, options);
-        fclose(fp);
-        if (node == NULL) return nil;
-        
-        _rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
-        _options = options;
-    }
-    return self;
+	if ((self = [super init])) {
+		FILE *fp = fopen(path.UTF8String, "r");
+		if (fp == NULL) return nil;
+		
+		cmark_node *node = cmark_parse_file(fp, options);
+		fclose(fp);
+		if (node == NULL) return nil;
+		
+		_rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
+		_options = options;
+	}
+	return self;
 }
-
+- (void)updateWithContentsOfFile:(NSString *)path options:(CMDocumentOptions)options
+{
+	FILE *fp = fopen(path.UTF8String, "r");
+	if (fp == NULL) return;
+	
+	cmark_node *node = cmark_parse_file(fp, options);
+	fclose(fp);
+	if (node == NULL) return;
+	
+	_rootNode = [[CMNode alloc] initWithNode:node freeWhenDone:YES];
+	_options = options;
+}
 @end
