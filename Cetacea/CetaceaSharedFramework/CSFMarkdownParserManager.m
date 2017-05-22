22adea8249f77c5bfe90584964de4ffa2356917c
Justin Fincher <zhtsu47@me.com>
2017-05-22 16:14:19 +0800

    Update Doc

--- Cetacea/CetaceaSharedFramework/CSFMarkdownParserManager.m
+++ Cetacea/CetaceaSharedFramework/CSFMarkdownParserManager.m
@@ -10,4 +10,31 @@
 
 @implementation CSFMarkdownParserManager
 
+#pragma mark Singleton Methods
+
++ (id)sharedManager {
+	static CSFMarkdownParserManager *sharedMyManager = nil;
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		sharedMyManager = [[self alloc] init];
+	});
+	return sharedMyManager;
+}
+
+- (id)init {
+	if (self = [super init])
+	{
+		self.paragraphParser = [TSBaseParser new];
+		self.fullParser = [TSBaseParser new];
+		self.lineParser = [TSBaseParser new];
+	}
+	return self;
+}
+
+- (void)dealloc
+{
+	
+}
+
+
 @end
