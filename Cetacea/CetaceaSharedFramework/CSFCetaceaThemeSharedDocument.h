41a8ff68499974b1f917c6390b5105b30e6934d5
Justin Fincher <zhtsu47@me.com>
2017-06-02 18:43:50 +0800

    Documentation

--- Cetacea/CetaceaSharedFramework/CSFCetaceaThemeSharedDocument.h
+++ Cetacea/CetaceaSharedFramework/CSFCetaceaThemeSharedDocument.h
@@ -35,19 +35,86 @@
 
 
 #pragma mark - Property
+
+/**
+ Default Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *defaultDataModel;
+@property (nonatomic,strong) NSDictionary<NSString *, id> *defaultTextAttributes;
+
+/**
+ Atx Header Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *atxHeaderDataModel;
+@property (nonatomic,strong) NSDictionary<NSString *, id> *atxHeaderTextAttributes;
+@property (nonatomic,strong) NSDictionary<NSString *, id> *atxHeaderTagAttributes;
+
+/**
+ Setext Header Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *setextHeaderDataModel;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *setextHeaderTextAttributes;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *setextHeaderTagAttributes;
+
+/**
+ Code Block Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *codeBlockDataModel;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *codeBlockTextAttributes;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *codeBlockTagAttributes;
+
+/**
+ Tab Indent Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *tabIndentDataModel;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *tabIndentTextAttributes;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *tabIndentTagAttributes;
+
+/**
+ Bold Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *boldDataModel;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *BoldTextAttributes;
+@property (nonatomic,strong)NSDictionary<NSString *, id> *BoldTagAttributes;
+
+/**
+ Italic Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *italicDataModel;
+
+/**
+ Strike Through Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *strikeThroughDataModel;
+
+/**
+ List Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *listDataModel;
+
+/**
+ Quote Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *quoteDataModel;
+
+/**
+ Image Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *imageDataModel;
+
+/**
+ Link Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *linkDataModel;
+
+/**
+ Editor View Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *editorViewDataModel;
+
+/**
+ Ruler View Markup
+ */
 @property (nonatomic,strong) CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel *ruleViewDataModel;
 
 @end
--- Cetacea/CetaceaSharedFramework/CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel.m
+++ Cetacea/CetaceaSharedFramework/CSFiCloudFileExtensionCetaceaThemeSyntaxMarkupModel.m
@@ -11,16 +11,25 @@
 @implementation CSFCetaceaThemeSyntaxMarkupModel
 
 #pragma mark - NSCoding
+
 - (instancetype)initWithCoder:(NSCoder *)aDecoder
 {
 	self = [super init];
 	if (!self) {
 		return nil;
 	}
+	self.textFrontColor = [aDecoder decodeObjectForKey:@"textFrontColor"];
+	self.textBackgroundColor = [aDecoder decodeObjectForKey:@"textBackgroundColor"];
+	self.markupFrontColor = [aDecoder decodeObjectForKey:@"markupFrontColor"];
+	self.markupBackgroundColor = [aDecoder decodeObjectForKey:@"markupBackgroundColor"];
 	return self;
 }
 - (void)encodeWithCoder:(NSCoder *)aCoder
 {
+	[aCoder encodeObject:self.textFrontColor forKey:@"textFrontColor"];
+	[aCoder encodeObject:self.textBackgroundColor forKey:@"textBackgroundColor"];
+	[aCoder encodeObject:self.markupFrontColor forKey:@"markupFrontColor"];
+	[aCoder encodeObject:self.markupBackgroundColor forKey:@"markupBackgroundColor"];
 }
 
 
@@ -35,10 +44,14 @@
 	if (!self) {
 		return nil;
 	}
+	self.dayMarkup = [aDecoder decodeObjectForKey:@"dayMarkup"];
+	self.nightMarkup = [aDecoder decodeObjectForKey:@"nightMarkup"];
 	return self;
 }
 - (void)encodeWithCoder:(NSCoder *)aCoder
 {
+	[aCoder encodeObject:self.dayMarkup forKey:@"dayMarkup"];
+	[aCoder encodeObject:self.nightMarkup forKey:@"nightMarkup"];
 }
 
 
